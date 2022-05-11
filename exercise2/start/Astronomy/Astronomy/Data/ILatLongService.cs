1a4c27b27ce3757e8c436269bc56f6fa26de0f50
Matt Soucoup <msoucoup@codemilltech.com>
2022-05-11 14:02:46 -0700

    initial add for new shell

---
+++ exercise2/start/Astronomy/Astronomy/Data/ILatLongService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomy
+{
+    public interface ILatLongService
+    {
+        Task<(double Latitude, double Longitude)> GetLatLong();
+    }
+}
---
+++ exercise3/start/Astronomy/Astronomy/Pages/AboutPage.xaml.cs
@@ -0,0 +1,9 @@
+namespace Astronomy.Pages;
+
+public partial class AboutPage : ContentPage
+{
+	public AboutPage()
+	{
+		InitializeComponent();
+	}
+}
