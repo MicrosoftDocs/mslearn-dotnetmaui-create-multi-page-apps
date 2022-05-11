f97ae978eb0336539fb456c560f6343cd84ba15b
Matt Soucoup <msoucoup@codemilltech.com>
2022-05-11 13:53:46 -0700

    Final version of exercise 1

---
+++ exercise2/final/Astronomy/Astronomy/Data/SunriseService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Text.Json;
+
+namespace Astronomy
+{
+    public class SunriseService
+    {
+        const string SunriseSunsetServiceUrl = "https://api.sunrise-sunset.org";
+
+        public async Task<(DateTime Sunrise, DateTime Sunset)> GetSunriseSunsetTimes(double latitude, double longitude)
+        {
+            var query = $"{SunriseSunsetServiceUrl}/json?lat={latitude}&lng={longitude}&date=today";
+
+            var client = new HttpClient();
+            client.DefaultRequestHeaders.Add("Accept", "application/json");
+            var json = await client.GetStringAsync(query);
+
+            var options = new JsonSerializerOptions { PropertyNameCaseInsensitive = true };
+            var data = JsonSerializer.Deserialize<SunriseSunsetData>(json, options);
+
+            return (DateTime.Parse(data.Results.Sunrise), DateTime.Parse(data.Results.Sunset));
+        }
+
+        class SunriseSunsetData
+        {
+#pragma warning disable 0649
+            // Field is only set via JSON deserialization, so disable warning that the field is never set.
+            //public SunriseSunsetResults Results;
+            public SunriseSunsetResults Results { get; set; }
+#pragma warning restore 0649
+        }
+
+        class SunriseSunsetResults
+        {
+#pragma warning disable 0649
+            // Fields are only set via JSON deserialization, so disable warning that the fields are never set.
+            public string Sunrise { get; set; }
+            public string Sunset { get; set; }
+#pragma warning restore 0649
+        }
+    }
+}
---
+++ exercise2/final/Astronomy/Astronomy/Pages/SunrisePage.xaml.cs
@@ -0,0 +1,40 @@
+namespace Astronomy.Pages;
+
+public partial class SunrisePage : ContentPage
+{
+    ILatLongService LatLongService { get; set; }
+    public SunrisePage()
+    {
+        InitializeComponent();
+        LatLongService = new LatLongService();
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        activityWaiting.IsRunning = true;
+        var sunriseSunsetData = await GetSunriseSunsetData();
+        InitializeUI(sunriseSunsetData.Item1, sunriseSunsetData.Item2, sunriseSunsetData.Item3);
+        activityWaiting.IsRunning = false;
+    }
+
+    async Task<(DateTime, DateTime, TimeSpan)> GetSunriseSunsetData()
+    {
+
+        var latLongData = await LatLongService.GetLatLong();
+        var sunData = await new SunriseService().GetSunriseSunsetTimes(latLongData.Latitude, latLongData.Longitude);
+
+        var riseTime = sunData.Sunrise.ToLocalTime();
+        var setTime = sunData.Sunset.ToLocalTime();
+        var span = setTime.TimeOfDay - riseTime.TimeOfDay;
+        return (riseTime, setTime, span);
+    }
+
+    void InitializeUI(DateTime riseTime, DateTime setTime, TimeSpan span)
+    {
+        lblDate.Text = DateTime.Today.ToString("D");
+        lblSunrise.Text = riseTime.ToString("h:mm tt");
+        lblDaylight.Text = $"{span.Hours} hours, {span.Minutes} minutes";
+        lblSunset.Text = setTime.ToString("h:mm tt");
+    }
+}
